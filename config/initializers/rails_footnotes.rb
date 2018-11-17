3c9c2ad7e36be7fe5735331940c450ef3ae06579
gusarov2k <motoface@yandex.ru>
2018-11-17 14:46:34 +0300

    add methods

--- config/initializers/rails_footnotes.rb
+++ config/initializers/rails_footnotes.rb
@@ -24,11 +24,7 @@ defined?(Footnotes) && Footnotes.setup do |f|
 
   # Allow to open multiple notes :
   # f.multiple_notes = true
+
 end
 
-# Footnotes.setup do |config|
-#   config.before {|controller, filter| filter.notes = controller.class.name =~ /Message/ && \
-#     controller.action_name == 'index' ? [:assigns] : []}
-#   config.before {|controller, filter| filter.notes |= [:params] if controller.class.name =~ /Profile/ && \
-#     controller.action_name == 'edit' }
-# end
+
