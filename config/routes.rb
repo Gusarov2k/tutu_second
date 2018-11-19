46b055fd72d2453b7b424868a7836596c88a929b
gusarov2k <motoface@yandex.ru>
2018-11-19 10:38:07 +0300

    add model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :railway_stations
+  resources :trains
   root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
