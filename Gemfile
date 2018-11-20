8ed769259394bf9fb6f0957fc7dd4ebbdc556a55
gusarov2k <motoface@yandex.ru>
2018-11-20 13:12:29 +0300

    install gem

--- Gemfile
+++ Gemfile
@@ -24,6 +24,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'slim'
 
+gem 'nested_form_fields'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/models/route.rb
+++ app/models/route.rb
@@ -1,7 +1,7 @@
 class Route < ActiveRecord::Base
   # https://medium.com/@oliviazyc/complex-nesting-forms-in-rails-631b369be0de
   # http://rusrails.ru/rails-form-helpers
-  #  http://railscasts.com/episodes/217-multistep-forms?autoplay=true
+  # http://railscasts.com/episodes/217-multistep-forms?autoplay=true
   validates :name, presence: :true
 
   has_many      :railway_station_routes
